fd637d292a6f1b41c79fdcfb5d8766dc5294440a
moad <moad.hassadi@gmail.com>
2022-03-02 21:39:51 +0100

    adding services and use of observble

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Hero } from '../hero';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
@@ -6,10 +8,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
+  //hero:Hero={id:1,name:"Windstorm",mod:false};
+ heroes:Hero[]=[];
+  selectedhero?:Hero;
 
-  constructor() { }
+
+  constructor(private heroservice:HeroService) { }
 
   ngOnInit(): void {
+    this.getHero();
   }
 
+  onSelect(hero:Hero){
+    this.selectedhero=hero;
+
+  }
+
+  //retruve hero from service
+  getHero():void{
+  this.heroservice.getHeroes().subscribe(heroes=>this.heroes=heroes);
+  }
 }
